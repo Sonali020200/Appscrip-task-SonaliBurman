e133574b76c6c9e73a96fc6edd21ca264b60aa66
Sonali Burman <sonali.b.020200@gmail.com>
2025-04-11 19:23:10 +0530

    edited

--- appscript/pages/index.js
+++ appscript/pages/index.js
@@ -5,45 +5,18 @@ import Filters from '../components/Filters';
 import ProductCard from '../components/ProductCard';
 import styles from '../styles/Home.module.css';
 import fetchProducts from '../utils/fetchProducts';
-import { useState } from 'react';
+
 
 export default function Home({ products }) {
-  const [selectedFilters, setSelectedFilters] = useState({});
-
-  const filters = [
-    { name: 'Category', options: ['Men', 'Women', 'Baby & Kids'] },
-  ];
-
-  const handleFilterChange = (filterName, value) => {
-    setSelectedFilters((prev) => {
-      const prevVals = prev[filterName] || [];
-      const updatedVals = prevVals.includes(value)
-        ? prevVals.filter((v) => v !== value)
-        : [...prevVals, value];
-      return { ...prev, [filterName]: updatedVals };
-    });
-  };
-
-  const filteredProducts = products.filter((product) => {
-    if (!selectedFilters.Category || selectedFilters.Category.length === 0)
-      return true;
-    return selectedFilters.Category.includes(product.category);
-  });
 
   return (
     <>
       <Head>
-        <title>Appscript - Product Listing</title>
-        
+        <title>Appscript - Product Listing</title>      
       </Head>
       <Header />
       <main className={styles.main}>
-        <Filters
-          filters={filters}
-          selectedFilters={selectedFilters}
-          onFilterChange={handleFilterChange}
-        />
-       
+        <Filters/>       
       </main>
       <Footer />
     </>
